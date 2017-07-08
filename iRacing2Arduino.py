05eba5e2603f5aec1bfacd3a7c6d3afd80b11466
Marc Matten <marc@matten-online.de>
2017-07-08 15:40:29 +0200

    - manual fan control when iRacing is not enabled - automatic identification of Arduino Port

--- iRacing2Arduino.py
+++ iRacing2Arduino.py
@@ -1,19 +1,35 @@
 import irsdk
 import serial
+import serial.tools.list_ports
 import struct
 from pygame import *
 
-ir = irsdk.IRSDK()
+# find serial port for Arduino Leonardo
+PortList = serial.tools.list_ports.comports()
+portFound = 0
+for i in range(1,len(PortList)):
+    if PortList[i].description[0:16] == 'Arduino Leonardo':
+        COMPort = PortList[i].device
+        portFound = 1
 
-ser = serial.Serial('COM4', 9600, timeout=1)
-time.wait(2000)
-ser.write(struct.pack('>B', 0))
+if portFound == 1:
+    print('Arduino found! Connecting to', COMPort)
 
-while ir.startup():
+    ser = serial.Serial('COM8', 9600, timeout=1)
+    time.wait(2000)
     ser.write(struct.pack('>B', 0))
 
-    if(ir['IsOnTrack']):
-        speed = ir['Speed']
-        fanSpeed = int(3.06*speed)
-        ser.write(struct.pack('>B', fanSpeed))
-        print(fanSpeed)
+    print('Connection established!')
+
+    ir = irsdk.IRSDK()
+    while ir.startup():
+        ser.write(struct.pack('>B', 0))
+
+        if(ir['IsOnTrack']):
+            speed = ir['Speed']
+            fanSpeed = int(3.06*speed)
+            ser.write(struct.pack('>B', fanSpeed))
+            print(fanSpeed)
+elif portFound == 0:
+    print('Arduino not found!... Terminating!')
+    quit()
--- iRacing_Fan/iRacing_Fan.ino
+++ iRacing_Fan/iRacing_Fan.ino
@@ -1,12 +1,12 @@
-// Pins
-int PotiPin = A0;
-int switchPin = 2;
-int PotiPin2 = A1;
-int switchPin2 = 4;
-int PWM1 = 10;
-int PWM2 = 11;
+// Pins for Fans
+int PotiPin = A2;
+int PotiPin2 = A3;
+int switchPin = 8;
+int switchPin2 = 2;
+int PWM1 = 3;
+int PWM2 = 9;
 
-// Variables
+// Variables for Fans
 int fan1;
 int fan2;
 int fanSpeed;
@@ -15,7 +15,45 @@ int Poti;
 int Switch2;
 int Poti2;
 
+// =========================================================
+//  G27 shifter pinout
+//
+//  DB9  Description        Pro Micro
+//  1    Clock              5
+//  2    Ourput Data        7
+//  3    Inpt Data          6
+//  4    Shifter X axis     A0
+//  5    LED                10
+//  6    GND                GND
+//  7    +5V                VCC
+//  8    Shifter Y axis     A1
+//  9    -                  -
+
+// include library that turns the Pro Mirco into a gamecontroller
+#include "Joystick.h"
+
+// Pro Micro pin definitions
+#define LED_PIN            10
+#define DATA_IN_PIN        7
+#define MODE_PIN           6
+#define CLOCK_PIN          5
+#define X_AXIS_PIN         A0
+#define Y_AXIS_PIN         A1
+
+// H-shifter mode analog axis thresholds
+#define HS_XAXIS_12        340
+#define HS_XAXIS_56        580
+#define HS_YAXIS_135       750
+#define HS_YAXIS_246       200
+
+// pin that controlls reverse gear
+#define DI_REVERSE         1
+
+// =========================================================
+// =========================================================
+
 void setup() {
+  // Setup for fan controll
   Serial.begin(9600);
   pinMode(switchPin, INPUT);
   pinMode(switchPin2, INPUT);
@@ -23,30 +61,58 @@ void setup() {
   pinMode(PotiPin2, INPUT);  
   pinMode(PWM1, OUTPUT);
   pinMode(PWM2, OUTPUT);
+  
+// =========================================================
+  Joystick.begin();
+  
+  // G27 shifter analog inputs configuration 
+  pinMode(X_AXIS_PIN, INPUT_PULLUP);   // X axis
+  pinMode(Y_AXIS_PIN, INPUT_PULLUP);   // Y axis
+  
+  // G27 shift register interface configuration 
+  pinMode(DATA_IN_PIN, INPUT);         // Data in
+  pinMode(MODE_PIN, OUTPUT);           // Parallel/serial mode
+  pinMode(CLOCK_PIN, OUTPUT);          // Clock
+  
+  // LED output mode configuration 
+  pinMode(LED_PIN, OUTPUT);            // LED
+     
+  // Digital outputs initialization
+  digitalWrite(LED_PIN, HIGH);
+  digitalWrite(MODE_PIN, HIGH);
+  digitalWrite(CLOCK_PIN, HIGH); 
+
+
+// =========================================================
+// =========================================================
 }
 
 void loop() {
-  // read data from Serial (iRacing/Python)
-  if (Serial.available()) {      
-    fanSpeed = Serial.read();
-  }
-  else{
-    fanSpeed = 60;
-  }
+  // Fan controll =========================================
 
   // read in Potis and Switches
   Poti = analogRead(PotiPin);
   Switch = digitalRead(switchPin);
   Poti2 = analogRead(PotiPin2);
-  Switch2 = digitalRead(switchPin2);
-
+  Switch2 = digitalRead(switchPin2);  
+    
   // remap poti values
   Poti = map(Poti, 0, 1023, 100, 255);
   Poti2 = map(Poti2, 0, 1023, 100, 255);
-
-  // map combined fan speed
-  fan1 = map(fanSpeed, 0, 255, 60, Poti);
-  fan2 = map(fanSpeed, 0, 255, 60, Poti2);
+  
+  // read data from Serial (iRacing/Python)
+  if (Serial.available()) {      
+    fanSpeed = Serial.read();
+  
+    // map combined fan speed
+    fan1 = map(fanSpeed, 0, 255, 60, Poti);
+    fan2 = map(fanSpeed, 0, 255, 60, Poti2);
+  }
+  else{
+    // manual fan speed controll
+    fan1 = Poti;
+    fan2 = Poti2;    
+  }
 
   // write data to output pins
   if (Switch==HIGH) {
@@ -60,6 +126,91 @@ void loop() {
   }else{
     digitalWrite(PWM2, 0);
   }
+
+  // G27 shifter ====================================================  
+  
+  // Reading of button states from G27 shift register
+  int b[16];
+  
+  digitalWrite(MODE_PIN, LOW);         // Parallel mode: inputs are read into shift register
+  delayMicroseconds(10);               // Wait for signal to settle
+  digitalWrite(MODE_PIN, HIGH);        // Serial mode: data bits are output on clock falling edge
+  
+  for(int i=0; i<16; i++)              // Iteration over both 8 bit registers
+  {
+    digitalWrite(CLOCK_PIN, LOW);      // Generate clock falling edge
+    delayMicroseconds(10);             // Wait for signal to settle
+  
+    b[i]=digitalRead(DATA_IN_PIN);     // Read data bit and store it into bit array
+    
+    digitalWrite(CLOCK_PIN, HIGH);     // Generate clock rising edge
+    delayMicroseconds(10);             // Wait for signal to settle
+  }
+
+  // Reading of shifter position
+  int x=analogRead(0);                 // X axis
+  int y=analogRead(1);                 // Y axis
+ 
+  // Current gear calculation
+  int gear=0;                          // Default value is neutral  
+  if(x<HS_XAXIS_12)                  // Shifter on the left?
+  {
+    if(y>HS_YAXIS_135) gear=1;       // 1st gear
+    if(y<HS_YAXIS_246) gear=2;       // 2nd gear
+  }
+  else if(x>HS_XAXIS_56)             // Shifter on the right?
+  {
+    if(y>HS_YAXIS_135) gear=5;       // 5th gear
+    if(y<HS_YAXIS_246) gear=6;       // 6th gear
+  }
+  else                               // Shifter is in the middle
+  {
+    if(y>HS_YAXIS_135) gear=3;       // 3rd gear
+    if(y<HS_YAXIS_246) gear=4;       // 4th gear
+  }
+
+  // check if in reverse gear
+  if(gear!=6) b[DI_REVERSE]=0;       // Reverse gear is allowed only on 6th gear position
+  if(b[DI_REVERSE]==1)               // 6th gear is deactivated if reverse gear is engaged
+  {
+    gear=7;                          // reverse gear
+    digitalWrite(LED_PIN, LOW);      // enable LED when reverse gear is selected
+  }
+  else
+  {
+    digitalWrite(LED_PIN, HIGH);     // disable LED if not in reverse gear
+  }
+
+// connect gear selection to G27 shifter button
+// gear 1-6 = button 0-5
+// reverse gear = button 6
+for (int i = 0; i < 7; i++)
+{
+  if (gear == i+1)
+  {
+    Joystick.pressButton(i);  
+  }
+  else
+  {
+    Joystick.releaseButton(i);  
+  }
+}
+
+// connect joystick button to G27 shifter buttons
+for (int k = 0; k < 12; k++)
+{
+  if (b[k+4] == 1)
+  {
+    Joystick.pressButton(k+7);  
+  }
+  else
+  {
+    Joystick.releaseButton(k+7);  
+  }
+}
+
+// send game controller state to PC
+Joystick.sendState();  
 }
 
 
